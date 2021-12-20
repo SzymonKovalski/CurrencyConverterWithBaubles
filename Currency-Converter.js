7d5a8f6bc64b4f156845419673aa910f32868c30
SzymonKovalski <kovalski.szymon@gmail.com>
2021-12-20 12:34:10 +0200

    Update Currency-Converter.js

--- Currency-Converter.js
+++ Currency-Converter.js
@@ -1,80 +1,39 @@
-const readline = require('readline');
-const rl = readline.createInterface({ input: process.stdin,
-                        output: process.stdout });
-//base euro, some external API would also work
-const currencyRate = [
-    ['GBP', 0.8],
-    ['JPY', 128.2],
-    ['BGN', 1.9],
-    ['CAD', 1.4],
-    ['HRK', 7.5],
-    ['NOK', 10.2],
-    ['USD', 1.1],
-    ['UAH', 30.9],
-    ['PLN', 4.6]
-];
-const countryNames = [
-    ['United States'],
-    ['Japan'],
-    ['Bulgaria'],
-    ['Canada'],
-    ['Croatia'],
-    ['Norway'],
-    ['England'],
-    ['Ukraine'],
-    ['Poland']
-];
-
-function getRate(Currency) {
-    return new Promise((resolve, reject) => {
-        for (let i = 0; i < currencyRate.length; i++) {
-            //console.log(i);
-            if (currencyRate[i][0] === Currency) {
-                i;
-                resolve(currencyRate[i][1]);
-            }
-        }
-        reject();
-    });
-}
-
-async function doExchangeRate(fromCurrency, toCurrency) {
-   //awaits rate, gives back exchangeRate
-   const fromRate = await getRate(fromCurrency);
-   const toRate = await getRate(toCurrency);
-   console.log(toRate / fromRate);
-}
-
-const getCountries = async toCurrency => {
-    const responce = await countryNames[toCurrency];
-    console.log(responce);
+// would probably have been an external API irl
+const currencyRate = {
+    'GBP': 0.8,
+    'JPY': 128.2,
+    'BGN': 1.9,
+    'CAD': 1.4,
+    'HRK': 7.5,
+    'NOK': 10.2,
+    'USD': 1.1,
+    'UAH': 30.9,
+    'PLN': 4.6,
 };
 
 
-rl.question('What would you like to do?', userInput => {
-    switch (userInput.trim()) {
-        case ('doExchangeRate'):
-            rl.on('line', from => {
-                rl.on('line', to => {
-                    doExchangeRate(from, to);
-                    rl.close();//emits a event
-                });
-            });
-            break;
-        case ('getCountries'):
-            rl.on('line', number => {
-                getCountries(number);
-                rl.close();
-            });
-            break;
-        case (''):
-            rl.close();
-            break;
-        default: rl.close();
-    }
-});
 
-//askAboutExchangeRates();
+async function getRateFromDatabase(Currency) {
+    const rate = currencyRate[Currency];
+    console.log(rate);
+    return rate;
+}
+async function doExchange(fromCurrency, toCurrency, value, Callback) {
+    //timer.start;
+    //faking a delay between database
 
-
-
+    const fromRate = getRateFromDatabase(fromCurrency);
+    const toRate = getRateFromDatabase(toCurrency);
+    console.log(fromRate);
+    console.log(toRate);
+    Promise.all([fromRate, toRate])
+    .then(([from, to]) => {
+        const result = value / from * to;
+        console.log(result);
+        return result;
+    })
+    .then(r => {
+        Callback(r);
+    })
+    .catch();
+}
