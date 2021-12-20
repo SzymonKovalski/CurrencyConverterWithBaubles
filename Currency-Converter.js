da2f906b533f09717080c119eb4f9640b3d887b4
SzymonKovalski <kovalski.szymon@gmail.com>
2021-12-20 12:36:33 +0200

    Update Currency-Converter.js

--- Currency-Converter.js
+++ Currency-Converter.js
@@ -1,23 +1,4 @@
-// would probably have been an external API irl
-const currencyRate = {
-    'GBP': 0.8,
-    'JPY': 128.2,
-    'BGN': 1.9,
-    'CAD': 1.4,
-    'HRK': 7.5,
-    'NOK': 10.2,
-    'USD': 1.1,
-    'UAH': 30.9,
-    'PLN': 4.6,
-};
 
-
-
-async function getRateFromDatabase(Currency) {
-    const rate = currencyRate[Currency];
-    console.log(rate);
-    return rate;
-}
 async function doExchange(fromCurrency, toCurrency, value, Callback) {
     //timer.start;
     //faking a delay between database
