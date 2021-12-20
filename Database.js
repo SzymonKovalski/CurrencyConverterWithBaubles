75cc9b84dae046891867b4eddf13a8b507955acb
SzymonKovalski <kovalski.szymon@gmail.com>
2021-12-20 13:23:01 +0200

    Update Database.js

--- Database.js
+++ Database.js
@@ -1,20 +1,25 @@
 // would probably have been an external API irl
-const currencyRate = {
-    'GBP': 0.8,
-    'JPY': 128.2,
-    'BGN': 1.9,
-    'CAD': 1.4,
-    'HRK': 7.5,
-    'NOK': 10.2,
-    'USD': 1.1,
-    'UAH': 30.9,
-    'PLN': 4.6,
-};
+class currencyRate {
+    constructor() {
+        this.GBP = 0.8;
+        this.JPY = 128.2;
+        this.BGN = 1.9;
+        this.CAD = 1.4;
+        this.HRK = 7.5;
+        this.NOK = 10.2;
+        this.USD = 1.1;
+        this.UAH = 30.9;
+        this.PLN = 4.6;
+    }
 
-
+}
 
 async function getRateFromDatabase(Currency) {
     const rate = currencyRate[Currency];
     console.log(rate);
     return rate;
 }
+
+getRateFromDatabase('UAH');
+export default currencyRate;
+export { getRateFromDatabase };
