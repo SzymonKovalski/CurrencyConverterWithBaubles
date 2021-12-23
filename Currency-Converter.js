95ce148b60df2a43c8475e09b8689cbb6f6b70e6
SzymonKovalski <kovalski.szymon@gmail.com>
2021-12-23 14:41:17 +0200

    Update Currency-Converter.js

--- Currency-Converter.js
+++ Currency-Converter.js
@@ -4,32 +4,27 @@ const fromAmountEL = document.getElementById('fromAmount');
 const toAmountEL = document.getElementById('toAmount');
 
 const rateEL = document.getElementById('rate');
-const swapEL = document.getElementById('Swap');
-
-// fetch currency rates
+const swapEL = document.getElementById('swap');
 
 function calculate() {
     const fromCurrency = fromCurrencyEL.value;
     const toCurrency = toCurrencyEL.value;
-
+    //returns a promise
     fetch(`https://v6.exchangerate-api.com/v6/e8c3ed2bdb2608062e5d5a95/latest/${fromCurrency}`)
     .then(res => res.json())
     .then(data => {
-        //console.log(data);
-        const rate = data.conversion_rates;
+        const rate = data.conversion_rates[toCurrency];
         rateEL.innerText = `1 ${fromCurrency} = ${rate} ${toCurrency}`;
 
         toAmountEL.value = (fromAmountEL.value * rate).toFixed(2);
     });
 }
 
-
-
-
-
-
-
-
+class Timer {
+    constructor(root) {
+        
+    }
+}
 
 
 
@@ -39,4 +34,10 @@ fromCurrencyEL.addEventListener('change', calculate);
 toCurrencyEL.addEventListener('change', calculate);
 fromAmountEL.addEventListener('input', calculate);
 toAmountEL.addEventListener('input', calculate);
+swapEL.addEventListener('click', () => {
+    const temp = fromCurrencyEL.value;
+    fromCurrencyEL.value = toCurrencyEL.value;
+    toCurrencyEL.value = temp;
+    calculate();
+  });
 calculate();
